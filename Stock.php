3dafa0597b961b1ed0411aadcb7235ef294c8c17
Karl Evans <karl.evans97@gmail.com>
2021-02-16 17:47:40 +0000

    Added initial Stock class

---
+++ Stock.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Stock class to keep information about a particular stock.
+ */
+
+class Stock {
+    /**
+     * @var string
+     */
+    public $symbol; # stock market ticker symbol
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * stock object constructor
+     *
+     * @param string $symbol the stock ticker symbol
+     * @param string $name the nameof the stock
+     * @access public
+     */
+    public function __construct($name, $ticker) {
+        $this->symbol = $ticker;
+        $this->name = $name;
+        echo "Created stock\n";
+    }
+
+}
+
+$s = new Stock("SPCE", "Virgin Galactic");
+echo $s->symbol;
+?>
